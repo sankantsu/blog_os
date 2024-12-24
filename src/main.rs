453a781bbd0b29c92e0e3e5b7daba5e7984f413f
Godai Takashina <azarashi.azarashi.a@gmail.com>
2024-12-25 00:45:38 +0900

    A minimal rust kernel

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@
 
 use core::panic::PanicInfo;
 
+static HELLO: &[u8] = b"Hello World!";
+
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
@@ -10,5 +12,12 @@ fn panic(_info: &PanicInfo) -> ! {
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(2 * i as isize) = byte;
+            *vga_buffer.offset(2 * i as isize + 1) = 0xb;
+        }
+    }
     loop {}
 }
