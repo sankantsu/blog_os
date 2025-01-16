1b50089a7745ea95251dcdc565ac142a72700c66
Godai Takashina <azarashi.azarashi.a@gmail.com>
2025-01-17 02:26:35 +0900

    feat: add serial_println! macro

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,6 @@ fn panic(_info: &PanicInfo) -> ! {
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     vga_buffer::print_something();
-    serial::print("Hello, serial port!");
+    serial_println!("Hello, serial port!");
     loop {}
 }
--- src/serial.rs
+++ src/serial.rs
@@ -10,10 +10,27 @@ lazy_static! {
     };
 }
 
-pub fn print(s: &str) {
+pub fn _print(args: ::core::fmt::Arguments) {
     use core::fmt::Write;
     SERIAL1
         .lock()
-        .write_str(s)
+        .write_fmt(args)
         .expect("Printing to serial failed");
+}
+
+/// Prints to the host through the serial interface.
+#[macro_export]
+macro_rules! serial_print {
+    ($($arg:tt)*) => {
+        $crate::serial::_print(format_args!($($arg)*));
+    };
+}
+
+/// Prints to the host through the serial interface, appending a newline.
+#[macro_export]
+macro_rules! serial_println {
+    () => ($crate::serial_print!("\n"));
+    ($fmt:expr) => ($crate::serial_print!(concat!($fmt, "\n")));
+    ($fmt:expr, $($arg:tt)*) => ($crate::serial_print!(
+        concat!($fmt, "\n"), $($arg)*));
 }
